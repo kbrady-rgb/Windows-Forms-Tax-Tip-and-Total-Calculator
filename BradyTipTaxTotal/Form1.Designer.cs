ad98284e920bf0bbca39fe7f26766cdb972cf3ef
kbrady-rgb <58741813+kbrady-rgb@users.noreply.github.com>
2020-09-15 15:27:11 -0500

    Completed project

---
+++ BradyTipTaxTotal/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace BradyTipTaxTotal
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblFood = new System.Windows.Forms.Label();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.lblOutput = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblFood
+            // 
+            this.lblFood.AutoSize = true;
+            this.lblFood.Location = new System.Drawing.Point(217, 58);
+            this.lblFood.Name = "lblFood";
+            this.lblFood.Size = new System.Drawing.Size(88, 17);
+            this.lblFood.TabIndex = 0;
+            this.lblFood.Text = "Cost of food:";
+
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Location = new System.Drawing.Point(29, 91);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(276, 17);
+            this.lblTotal.TabIndex = 1;
+            this.lblTotal.Text = "After adding sales tax and tip, your total is:";
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(311, 53);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(100, 22);
+            this.txtInput.TabIndex = 1;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(185, 136);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(84, 59);
+            this.btnCalculate.TabIndex = 2;
+            this.btnCalculate.Text = "&Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.BtnCalculate_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnExit.Location = new System.Drawing.Point(367, 191);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(84, 29);
+            this.btnExit.TabIndex = 3;
+            this.btnExit.Text = "&Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.BtnExit_Click);
+            // 
+            // lblOutput
+            // 
+            this.lblOutput.AutoSize = true;
+            this.lblOutput.Location = new System.Drawing.Point(311, 91);
+            this.lblOutput.Name = "lblOutput";
+            this.lblOutput.Size = new System.Drawing.Size(0, 17);
+            this.lblOutput.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnCalculate;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.CancelButton = this.btnExit;
+            this.ClientSize = new System.Drawing.Size(463, 232);
+            this.Controls.Add(this.lblOutput);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.lblFood);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblFood;
+        private System.Windows.Forms.Label lblTotal;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label lblOutput;
+    }
+}
+
---
+++ BradyTipTaxTotal/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/***************************************************************
+* Name        : Tip, Tax, and Total Program
+* Author      : Kabrina Brady
+* Created     : 9/10/2019
+* Course      : CIS 169 - C#
+* Version     : 1.0
+* OS          : Windows 10
+* Copyright   : This is my own original work
+* Description : Program asks for cost of food from user, adds 15% tip and 7% sales tax, and displays the result
+*               Input:  Cost of food
+*               Output: Cost of food plus tax and tip
+* Academic Honesty: I attest that this is my original work.
+* I have not used unauthorized source code, either modified or unmodified. I have not given other fellow student(s) access to my program.         
+***************************************************************/
+
+namespace BradyTipTaxTotal
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnExit_Click(object sender, EventArgs e)
+        {
+            this.Close(); //exit button terminates program
+        }
+
+        private void BtnCalculate_Click(object sender, EventArgs e)
+        {
+            double foodPrice;
+            double SALES_TAX = 0.07;
+            double thisFoodSalesTax; //holds value of tax for this specific meal
+            double TIP = 0.15;
+            double thisFoodTip; //holds value of tip for this specific meal
+            double totalCost;
+
+            //get the cost of food and convert to double
+            foodPrice = double.Parse(txtInput.Text);
+
+            //calculate sales tax of this specific meal
+            thisFoodSalesTax = foodPrice * SALES_TAX;
+
+            //calculate tip for this specific meal
+            thisFoodTip = foodPrice * TIP;
+
+            //add original price, sales tax, and tip
+            totalCost = foodPrice + thisFoodSalesTax + thisFoodTip;
+
+            //convert totalCost to string and display in label
+            lblOutput.Text = totalCost.ToString("c");
+        }
+    }
+}
